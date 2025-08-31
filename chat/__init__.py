89cf6ed8aa7c7d121bf7c5741c694db66a45492b
Ananda S RPi <4833102+asarangaram@users.noreply.github.com>
2025-08-31 09:53:11 +0530

    add docker with nginx and gunicorn front

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,7 +1,21 @@
 #!/bin/env python
+
+from gevent import monkey
+monkey.patch_all() 
+
+
 from app import create_app, socketio
+import os
 
-app = create_app(debug=True)
+application = create_app(debug=True)
 
-if __name__ == '__main__':
-    socketio.run(app)
+# Redis for inter-worker communication
+redis_url = os.getenv("REDIS_URL", None)
+
+if redis_url:
+    socketio.init_app(application, cors_allowed_origins="*", message_queue=redis_url, async_mode="gevent")
+else:
+    socketio.init_app(application)
+
+if __name__ == "__main__":
+    socketio.run(application, host="0.0.0.0", port=5000)
